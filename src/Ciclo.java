5d6216d7154998f1e0288f21fd3ee366aa9e46b1
Drace97 <drace@hotmail.es>
2025-03-28 10:29:15 +0100

    Cambios y comentarios con javadoc.

--- src/Ciclo.java
+++ src/Ciclo.java
@@ -1,19 +1,37 @@
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Clase que representa a un ciclo educativo.
+ */
 public class Ciclo {
     private String nombreCiclo;
     private List<Estudiante> listaEstudiantes;
 
+    /**
+     * Constructor de la clase Ciclo.
+     *
+     * @param nombreCiclo Nombre del ciclo educativo
+     */
     public Ciclo(String nombreCiclo) {
         this.nombreCiclo = nombreCiclo;
         this.listaEstudiantes = new ArrayList<>();
     }
 
+    /**
+     * Agrega un estudiante al ciclo.
+     *
+     * @param e Estudiante a agregar
+     */
     public void agregarEstudiante(Estudiante e) {
         listaEstudiantes.add(e);
     }
 
+    /**
+     * Obtiene los datos del ciclo.
+     *
+     * @return Datos del ciclo como cadena
+     */
     public String datosCiclo() {
         StringBuilder datos = new StringBuilder("Ciclo: " + nombreCiclo + "\nEstudiantes:\n");
         for (Estudiante e : listaEstudiantes) {
@@ -26,4 +44,3 @@ public class Ciclo {
         return nombreCiclo;
     }
 }
-
